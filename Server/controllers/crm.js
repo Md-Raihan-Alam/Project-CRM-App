9e2fe1c173f917ab47ad15865e3a0a0e9355a076
Md-Raihan-Alam <mdraihanalam40@gmail.com>
2023-06-26 16:42:23 +0600

    CRM route updated

--- Server/controllers/crm.js
+++ Server/controllers/crm.js
@@ -1,18 +1,48 @@
+const Client = require("../models/people");
 const { StatusCodes } = require("http-status-codes");
-const createPerson = (req, res) => {
-  res.send("Created Person");
+const { findOneAndUpdate } = require("../models/users");
+const createPerson = async (req, res) => {
+  req.body.createdBy = req.user.userId;
+  const poeple = await Client.create(req.body);
+  res.status(StatusCodes.CREATED).json({ poeple });
 };
-const getAllPerson = (req, res) => {
-  res.send("Get all person");
+const getAllPerson = async (req, res) => {
+  const peoples = await Client.find({ createdBy: req.user.userId }).sort(
+    "createdAt"
+  );
+  res.send(StatusCodes.OK).json({ peoples, count: peoples.length });
 };
-const getSinglePerson = (req, res) => {
-  res.send("Getting a single person");
+const getSinglePerson = async (req, res) => {
+  const {
+    user: userId,
+    params: { id: peopleId },
+  } = req;
+  const people = await Client.findOne({ createdBy: userId, _id: peopleId });
+  res.status(StatusCodes.OK).json({ people });
 };
-const updatePerson = (req, res) => {
-  res.send("Updating a person");
+const updatePerson = async (req, res) => {
+  const {
+    body: { name, address, city, state, zipcode },
+    user: { userId },
+    params: { id: peopleId },
+  } = req;
+  const people = await findOneAndUpdate(
+    { _id: peopleId, createdBy: userId },
+    req.body,
+    { new: true, runValidators: true }
+  );
+  res.status(StatusCodes.OK).json({ people });
 };
-const deletePerson = (req, res) => {
-  res.send("Delete person");
+const deletePerson = async (req, res) => {
+  const {
+    user: userId,
+    params: { id: peopleId },
+  } = req;
+  const people = await Client.findByIdAndRemove({
+    _id: peopleId,
+    createdBy: userId,
+  });
+  res.status(StatusCodes.OK).send();
 };
 module.exports = {
   createPerson,
--- Server/middleware/authentications.js
+++ Server/middleware/authentications.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 const auth = async (req, res, next) => {
   const authHeader = req.headers.authorization;
-  const toke = authHeader.split(" ")[1];
+  const token = authHeader.split(" ")[1];
   try {
     const payload = jwt.verify(token, process.env.JWT_SECRET);
     res.user = { userId: payload.userId, name: payload.name };
